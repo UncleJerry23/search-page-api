c8af3119ee066e48f73eb57946b58d51f7ec88f9
Jared Myhrberg <jared.myhrberg@gmail.com>
2019-03-05 21:15:41 -0800

    hash tests passing for write search, write page and read query

--- test/hash-query.test.js
+++ test/hash-query.test.js
@@ -29,3 +29,61 @@ test('update existing hash', assert => {
     // assert
     assert.equal(result, 'searchTerm=harry+potter&page=1');
 });
+
+
+
+
+function writePageToQuery(existingQuery, page) {
+    const searchParams = new URLSearchParams(existingQuery);
+    searchParams.set('page', page);
+
+    return searchParams.toString();
+}
+
+test('write page to query', assert => {
+    // arrange
+    const existingQuery = 'searchTerm=star+wars&page=5';
+    const page = 2;
+    // act
+    const result = writePageToQuery(existingQuery, page); 
+    // assert
+    assert.equal(result, 'searchTerm=star+wars&page=2');
+});
+
+
+function readFromQuery(query) {
+    const searchParams = new URLSearchParams(query);
+    const pageNumber = parseInt(searchParams.get('page')) || 1;
+    const result = {
+        searchTerm: searchParams.get('searchTerm'),
+        page: pageNumber
+    };
+    return result;
+}
+
+
+test('reads options from query', assert => {
+    // arrange
+    const query = 'searchTerm=star+wars&page=3';
+    const expected = {
+        searchTerm: 'star wars',
+        page: 3
+    };
+    // act
+    const results = readFromQuery(query);
+    // assert
+    assert.deepEqual(results, expected);
+});
+
+test('if query has no page return 1', assert => {
+    // arrange
+    const query = 'searchTerm=harry+potter';
+    const expected = {
+        searchTerm: 'harry potter',
+        page: 1
+    };
+    // act
+    const result = readFromQuery(query);
+    // assert
+    assert.deepEqual(result, expected);
+});
